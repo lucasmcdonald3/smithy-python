c77c82926918d6ade81218b7667a9b2355def2a6
Lucas McDonald <lucmcdon@amazon.com>
2023-09-20 10:55:21 -0700

    make settings public

--- codegen/smithy-python-codegen/src/main/java/software/amazon/smithy/python/codegen/PythonSettings.java
+++ codegen/smithy-python-codegen/src/main/java/software/amazon/smithy/python/codegen/PythonSettings.java
@@ -31,7 +31,7 @@ import software.amazon.smithy.utils.SmithyUnstableApi;
  * TODO: make this immutable
  */
 @SmithyUnstableApi
-public final class PythonSettings {
+public class PythonSettings {
 
     private static final String SERVICE = "service";
     private static final String MODULE_NAME = "module";
